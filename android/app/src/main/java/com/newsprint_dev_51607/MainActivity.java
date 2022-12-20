ca55a77439df44a6e5ca6227809f29ee399c0271
Crowdbotics <engineering@crowdbotics.com>
2022-12-20 12:42:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/newsprint_dev_51607/MainActivity.java
@@ -0,0 +1,15 @@
+package com.newsprint_dev_51607;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "newsprint_dev_51607";
+  }
+}
